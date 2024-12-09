dc872316d65a06efdbe41acee1c624adb172b530
Ujhyi <david.ujh@gmail.com>
2024-12-09 08:44:30 +0100

    update

--- src/app/service/master.service.ts
+++ src/app/service/master.service.ts
@@ -7,38 +7,57 @@ import {Observable} from 'rxjs';
 })
 export class MasterService {
 
+
+  // LOCALHOST
+
+
+
   private monitorUrl = 'http://localhost:51834/WebService.asmx/GetMonitors';
   private televisionsUrl = 'http://localhost:51834/WebService.asmx/GetTelevision';
   private projectorsUrl = 'http://localhost:51834/WebService.asmx/GetProjector';
-
   private addNewDeviceUrl = 'http://localhost:51834/WebService.asmx/AddProduct';
   private deleteDeviceUrl = 'http://localhost:51834/WebService.asmx/DeleteProduct';
   private sellDeviceUrl = 'http://localhost:51834/WebService.asmx/SellProducts';
   private editDeviceUrl = 'http://localhost:51834/WebService.asmx/EditDevice';
-
   private countDevicesUrl = 'http://localhost:51834/WebService.asmx/CountDevices';
   private countDeviceByDateUrl = 'http://localhost:51834/WebService.asmx/GetStockInformation_StartDateToEndDate';
   private countDeviceByDateDetailTech1 = 'http://localhost:51834/WebService.asmx/GetStockInformation_StartDateToEndDate_1';
   private countDeviceByDateDetailTech2 = 'http://localhost:51834/WebService.asmx/GetStockInformation_StartDateToEndDate_2';
-
-  //private countModel = 'http://localhost:51834/WebService.asmx/GetModelCounts';
-  // Sales Revenue
+  private countModel = 'http://localhost:51834/WebService.asmx/GetModelCounts';
   private salesRevenueUrl = 'http://localhost:51834/WebService.asmx/SalesRevenue';
-  //Sales Revenue By Date
   private salesRevenueByDateUrl = 'http://localhost:51834/WebService.asmx/CalculateRevenueByDate';
-  //Display Sales Logs
   private salesLogsUrl = 'http://localhost:51834/WebService.asmx/DisplaySalesLogs';
-  // Display Filter
   private filterDeviceUrl = 'http://localhost:51834/WebService.asmx/GetFilteredProducts';
-  // Read All XML
   private getAll = 'http://localhost:51834/WebService.asmx/ReadXML';
 
+
+
+  // APACHE2
+
+  /*
+
+  private monitorUrl = 'http://20.64.238.96/MyASMXService/WebService.asmx/GetMonitors';
+  private televisionsUrl = 'http://20.64.238.96/MyASMXService/WebService.asmx/GetTelevision';
+  private projectorsUrl = 'http://20.64.238.96/MyASMXService/WebService.asmx/GetProjector';
+  private sellDeviceUrl = 'http://20.64.238.96/MyASMXService/WebService.asmx/SellProducts';
+  private deleteDeviceUrl = 'http://20.64.238.96/MyASMXService/WebService.asmx/DeleteProduct';
+
+
+*/
+
+
   constructor(private http: HttpClient) { }
+
+
+
+
+  // LOCALHOST
+
+
 
   getMonitors(): Observable<any> { return this.http.post(this.monitorUrl, null, { responseType: 'text' }); }
   getTelevisions(): Observable<any> { return this.http.post(this.televisionsUrl, null, { responseType: 'text' }); }
   getProjectors(): Observable<any> { return this.http.post(this.projectorsUrl, null, { responseType: 'text' }); }
-
   deleteProduct(productType: string, id: string): Observable<any> { return this.http.post<string>(this.deleteDeviceUrl, { productType, id }); }
   sellDevice(productType: string, id: string): Observable<any> { return this.http.post(this.sellDeviceUrl, { productType, id }); }
   addNewDevice(productType: string, brand: string, model: string, displaySize: string, refreshRate: string, displayTech: string, maxResolutions: string,
@@ -53,7 +72,6 @@ export class MasterService {
     return this.http.post(this.editDeviceUrl, {id, productType, brand, displaySize, refreshRate, displayTech, maxResolutions, stock, pixelResolutions, connections,
       output, speaker, smart, os, bluetooth, wirelessConnections, productionYear, VESA, color, price, stockDate});
   }
-
   getSalesLogs(): Observable<any> { return this.http.post(this.salesLogsUrl, null, { responseType: 'text' }); }
   getSalesRevenue(): Observable<any> { return this.http.post(this.salesRevenueUrl, null, { responseType: 'text' }); }
   calculateRevenueByDate(startDate: string, endDate: string): Observable<string> {
@@ -61,20 +79,17 @@ export class MasterService {
     const body = `startDate=${encodeURIComponent(startDate)}&endDate=${encodeURIComponent(endDate)}`;
     return this.http.post(this.salesRevenueByDateUrl, body, { headers, responseType: 'text' });
   }
-
   countDevice(): Observable<any> { return this.http.post(this.countDevicesUrl, null, { responseType: 'text' }); }
   countDeviceByDate(startDate: string, endDate: string): Observable<string>{
     const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' });
     const body = `startDate=${encodeURIComponent(startDate)}&endDate=${encodeURIComponent(endDate)}`;
     return this.http.post(this.countDeviceByDateUrl, body, { headers, responseType: 'text' });
   }
-
   getStockInformationByDateRange(startDate: string, endDate: string): Observable<string> {
     const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' });
     const body = `startDate=${encodeURIComponent(startDate)}&endDate=${encodeURIComponent(endDate)}`;
     return this.http.post(this.countDeviceByDateUrl, body, { headers, responseType: 'text' });
   }
-
   getFilteredProducts(params: any): Observable<string> {
     const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' });
     const body = `productType=${encodeURIComponent(params.productType)}&` +
@@ -85,17 +100,49 @@ export class MasterService {
       `sortByPrice=${encodeURIComponent(params.sortByPrice)}`;
     return this.http.post(this.filterDeviceUrl, body, { headers, responseType: 'text' });
   }
-
   getStockInformationByDateRangeTechInf1(startDate: string, endDate: string): Observable<string> {
     const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' });
     const body = `startDate=${encodeURIComponent(startDate)}&endDate=${encodeURIComponent(endDate)}`;
     return this.http.post(this.countDeviceByDateDetailTech1, body, { headers, responseType: 'text' });
   }
-
   getStockInformationByDateRangeTechInf2(startDate: string, endDate: string): Observable<string> {
     const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' });
     const body = `startDate=${encodeURIComponent(startDate)}&endDate=${encodeURIComponent(endDate)}`;
     return this.http.post(this.countDeviceByDateDetailTech2, body, { headers, responseType: 'text' });
   }
+
+
+
+
+  // APACHE2
+
+  /*
+
+  getMonitors(): Observable<any> { return this.http.post(this.monitorUrl, null, { responseType: 'text' }); }
+  getTelevisions(): Observable<any> { return this.http.post(this.televisionsUrl, null, { responseType: 'text' }); }
+  getProjectors(): Observable<any> { return this.http.post(this.projectorsUrl, null, { responseType: 'text' }); }
+
+  sellDevice(productType: string, id: string): Observable<any> {
+    const body = new URLSearchParams();
+    body.set('productType', productType);
+    body.set('id', id);
+
+    return this.http.post(this.sellDeviceUrl, body.toString(), {
+      headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
+    });
+  }
+
+  deleteProduct(productType: string, id: string): Observable<any> {
+    const body = new URLSearchParams();
+    body.set('productType', productType);
+    body.set('id', id);
+
+    return this.http.post(this.deleteDeviceUrl, body.toString(), {
+      headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
+    });
+  }
+
+
+   */
 
 }
